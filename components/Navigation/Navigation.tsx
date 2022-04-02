a03a3d678418d205cdeb78f0d67abeec06ac2c8e
janimaenpaa <itsmayd@gmail.com>
2022-04-02 16:41:24 +0300

    Add Navigation

---
+++ components/Navigation/Navigation.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const Navigation = (props: Props) => {
+  return (
+    <div className="flex fixed items-center bg-black h-16 w-full text-white p-10">
+      Navigation
+    </div>
+  );
+};
+
+export default Navigation;
