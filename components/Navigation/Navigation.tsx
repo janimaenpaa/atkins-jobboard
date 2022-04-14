7e6c0d0032e71d6f6e0682c6d04fa95a390f8510
janimaenpaa <itsmayd@gmail.com>
2022-04-14 16:44:26 +0300

    Make Navigation sticky

--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -4,7 +4,7 @@ type Props = {};
 
 const Navigation = (props: Props) => {
   return (
-    <div className="flex fixed items-center bg-black h-16 w-full text-white p-10">
+    <div className="flex sticky items-center bg-black h-16 w-full text-white p-10">
       Navigation
     </div>
   );
