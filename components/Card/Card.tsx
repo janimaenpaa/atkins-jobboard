7c9dc43c648abb21d78dc8e691a28af44af171c5
janimaenpaa <itsmayd@gmail.com>
2022-04-15 15:42:09 +0300

    Add style changes to Card

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -3,16 +3,24 @@ import React from "react";
 type Props = {
   children: React.ReactNode;
   title?: string;
+  subtitle?: string;
   margin?: string;
   padding?: string;
 };
 
-const Card = ({ children, title, margin = "4", padding = "4" }: Props) => {
+const Card = ({
+  children,
+  title,
+  subtitle,
+  margin = "4",
+  padding = "4",
+}: Props) => {
   return (
     <div
       className={`w-full m-${margin} p-${padding} border-gray-500 bg-white rounded-md`}
     >
-      {title && <h3 className="text-xl font-bold my-1">{title}</h3>}
+      {title && <h3 className="text-xl font-bold mt-1">{title}</h3>}
+      {subtitle && <h4 className="text-stone-600 font-semibold ">{subtitle}</h4>}
       {children}
     </div>
   );
