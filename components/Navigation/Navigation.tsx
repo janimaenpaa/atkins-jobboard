af9f90c52cc4ed0cfb439002540a12606609b0d8
janimaenpaa <itsmayd@gmail.com>
2022-04-15 15:56:12 +0300

    Add links to Navigation

--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -1,11 +1,17 @@
+import Link from "next/link";
 import React from "react";
 
 type Props = {};
 
 const Navigation = (props: Props) => {
   return (
-    <div className="flex sticky items-center bg-black h-16 w-full text-white p-10">
-      Navigation
+    <div className="flex sticky items-center justify-between bg-black h-16 w-full text-white p-10">
+      <Link href="/">
+        <a className="text-lg font-bold">Atkins Jobs</a>
+      </Link>
+      <Link href="/postjob">
+        <a className="font-semibold">Post A Job</a>
+      </Link>
     </div>
   );
 };
