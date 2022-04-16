7cd7b3807268a6a3ce210e4d64d3f7919409cebe
janimaenpaa <itsmayd@gmail.com>
2022-04-16 20:05:18 +0300

    Add error messages to Input fields

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -1,23 +1,41 @@
 import React from "react";
+import { FieldError } from "react-hook-form";
 
 type Props = {
   label?: string;
   type?: string;
+  error?: FieldError;
   register?: any;
 };
 
-const Input = ({ label, type = "text", register }: Props) => {
+const Input = ({ label, type = "text", error, register }: Props) => {
   return (
-    <div className="my-2">
+    <div className="flex flex-col my-2">
       {label && <label>{label}</label>}
+      {error && error.type === "required" && (
+        <span className="text-red-500 text-sm">This field is required.</span>
+      )}
+      {error && error.type === "pattern" && (
+        <span className="text-red-500 text-sm">
+          Enter a valid email address.
+        </span>
+      )}
       {type === "textarea" ? (
         <textarea
-          className="border border-slate-500 rounded-md p-2 my-2 w-full"
+          className={
+            error
+              ? "border border-red-500 rounded-md p-2 my-2 w-full"
+              : "border border-slate-500 rounded-md p-2 my-2 w-full"
+          }
           {...register}
         ></textarea>
       ) : (
         <input
-          className="border border-slate-500 rounded-md p-2 my-2 w-full"
+          className={
+            error
+              ? "border border-red-500 rounded-md p-2 my-2 w-full"
+              : "border border-slate-500 rounded-md p-2 my-2 w-full"
+          }
           type={type}
           {...register}
         />
--- pages/postjob/index.tsx
+++ pages/postjob/index.tsx
@@ -20,16 +20,43 @@ const PostJob = (props: Props) => {
       <Card>
         <h2 className="text-2xl font-bold mb-4">Post A Job</h2>
         <form className="p-0 m-0" onSubmit={onSubmit}>
-          <Input register={{ ...register("company") }} label="Company" />
-          <Input register={{ ...register("name") }} label="Name" />
-          <Input register={{ ...register("email") }} label="Email" />
-          <Input register={{ ...register("title") }} label="Job title" />
           <Input
-            register={{ ...register("description") }}
-            label="Description"
-            type="textarea"
+            register={{ ...register("company", { required: true }) }}
+            label="Company*"
+            error={errors?.company}
           />
-          <Input register={{ ...register("url") }} label="Link for applying" />
+          <Input
+            register={{ ...register("name", { required: true }) }}
+            label="Name*"
+            error={errors?.name}
+          />
+          <Input
+            register={{
+              ...register("email", {
+                required: true,
+                pattern:
+                  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+              }),
+            }}
+            label="Email*"
+            error={errors?.email}
+          />
+          <Input
+            register={{ ...register("title", { required: true }) }}
+            label="Job title*"
+            error={errors?.title}
+          />
+          <Input
+            register={{ ...register("description", { required: true }) }}
+            label="Description*"
+            type="textarea"
+            error={errors?.description}
+          />
+          <Input
+            register={{ ...register("url", { required: true }) }}
+            label="Link for applying*"
+            error={errors?.url}
+          />
           <Input register={{ ...register("deadline") }} label="Deadline" />
           <button
             className="w-full bg-sky-500 p-2 text-white rounded-md"
