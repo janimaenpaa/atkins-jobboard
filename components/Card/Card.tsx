4b977effd741a2fb213510e8fe62a535d8e0713d
janimaenpaa <itsmayd@gmail.com>
2022-04-16 15:33:19 +0300

    Add tailwind fixes to card

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -4,24 +4,16 @@ type Props = {
   children: React.ReactNode;
   title?: string;
   subtitle?: string;
-  margin?: string;
-  padding?: string;
 };
 
-const Card = ({
-  children,
-  title,
-  subtitle,
-  margin = "4",
-  padding = "4",
-}: Props) => {
-  const p = `p-${padding}`;
-  const m = `m-${margin}`;
+const Card = ({ children, title, subtitle }: Props) => {
   return (
-    <div className={`w-full max-w-6xl ${m} ${p} border-gray-500 bg-white rounded-md`}>
+    <div
+      className={`w-full max-w-6xl m-2 p-4 border-gray-500 bg-white rounded-md`}
+    >
       {title && <h3 className="text-xl font-bold mt-1">{title}</h3>}
       {subtitle && (
-        <h4 className="text-stone-600 font-semibold ">{subtitle}</h4>
+        <h4 className="text-stone-600 font-semibold">{subtitle}</h4>
       )}
       {children}
     </div>
