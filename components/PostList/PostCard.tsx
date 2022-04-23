b1ec00a9d1fb6e79e06753ba91fcb9cf2aa82507
janimaenpaa <itsmayd@gmail.com>
2022-04-23 12:28:27 +0300

    Remove unused imports

--- components/PostList/PostCard.tsx
+++ components/PostList/PostCard.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Post } from "@prisma/client";
 import Card from "../Card";
 import SkillBar from "../SkillBar";
-import { format, formatDistance, formatRelative, subDays } from 'date-fns'
+import { format } from 'date-fns'
 
 type Props = {
   post: Post;
