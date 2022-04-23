2e82c323194ddc99cf4ad69b10e884acb8c52a30
janimaenpaa <itsmayd@gmail.com>
2022-04-23 12:27:59 +0300

    Format dates on PostCard

--- components/PostList/PostCard.tsx
+++ components/PostList/PostCard.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Post } from "@prisma/client";
 import Card from "../Card";
 import SkillBar from "../SkillBar";
+import { format, formatDistance, formatRelative, subDays } from 'date-fns'
 
 type Props = {
   post: Post;
@@ -11,7 +12,7 @@ const PostCard = ({ post }: Props) => {
   return (
     <Card
       title={post.title}
-      subtitle={`${post.company} - Published on ${post.updatedAt}`}
+      subtitle={`${post.company} - Published on ${format(post.updatedAt, "dd.MM.yyyy")}`}
     >
       <SkillBar
         required={post.requiredSkills}
