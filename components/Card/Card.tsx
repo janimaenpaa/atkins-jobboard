39489f48c1c505fd8181f189d11eb8932fef1946
janimaenpaa <itsmayd@gmail.com>
2022-05-04 19:04:34 +0300

    Add fullWidth prop to Card

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -4,17 +4,20 @@ type Props = {
   children: React.ReactNode;
   title?: string;
   subtitle?: string;
+  fullWidth?: boolean;
 };
 
-const Card = ({ children, title, subtitle }: Props) => {
+const Card = ({ children, title, subtitle, fullWidth }: Props) => {
   return (
     <div
-      className={`w-full max-w-6xl m-2 p-4 border-gray-500 bg-white rounded-md`}
+      className={
+        fullWidth
+          ? `w-full m-2 p-4 border-gray-500 bg-white rounded-md  overflow-x-auto min-w-full`
+          : `w-full max-w-6xl m-2 p-4 border-gray-500 bg-white rounded-md`
+      }
     >
       {title && <h3 className="text-xl font-bold mt-1">{title}</h3>}
-      {subtitle && (
-        <h4 className="text-stone-600 font-semibold">{subtitle}</h4>
-      )}
+      {subtitle && <h4 className="text-stone-600 font-semibold">{subtitle}</h4>}
       {children}
     </div>
   );
