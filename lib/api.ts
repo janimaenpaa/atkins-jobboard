da42dec23a392b47557eebc249538ff9f8ebd734
janimaenpaa <itsmayd@gmail.com>
2022-05-04 19:03:00 +0300

    Add API helpers

---
+++ lib/api.ts
@@ -0,0 +1,36 @@
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+export const GET = async (path: string) => {
+  const response = await fetch(`${API_URL}/${path}`);
+
+  return await response.json();
+};
+
+export const POST = async (path: string, data: object) => {
+  const response = await fetch(`${API_URL}/${path}`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data),
+  });
+
+  return await response.json();
+};
+
+export const PUT = async (path: string, data: object) => {
+  const response = await fetch(`${API_URL}/${path}`, {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data),
+  });
+
+  return await response.json();
+};
+
+export const DELETE = async (path: string) => {
+  const response = await fetch(`${API_URL}/${path}`, {
+    method: 'DELETE',
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  return await response.json();
+};
