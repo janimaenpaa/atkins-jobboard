38f8baea4b2df31bb1fc5251796b75e5e9d865db
janimaenpaa <itsmayd@gmail.com>
2022-05-04 19:15:14 +0300

    Add different view for Admin

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,18 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Fragment } from "react";
 import Navigation from "../components/Navigation";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const { pathname } = useRouter();
+
+  if (pathname.includes("/admin"))
+    return (
+      <Fragment>
+        <Component {...pageProps} />
+      </Fragment>
+    );
+
   return (
     <Fragment>
       <Navigation />
