5ceff51e6f952c7fbc0c30ec37e34b03e6986203
janimaenpaa <itsmayd@gmail.com>
2022-05-07 14:58:14 +0300

    Add titleHref prop

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,22 +1,33 @@
+import Link from "next/link";
 import React from "react";
 
 type Props = {
   children: React.ReactNode;
   title?: string;
+  titleHref?: string;
   subtitle?: string;
   fullWidth?: boolean;
 };
 
-const Card = ({ children, title, subtitle, fullWidth }: Props) => {
+const Card = ({ children, title, titleHref, subtitle, fullWidth }: Props) => {
   return (
     <div
       className={
         fullWidth
-          ? `w-full m-2 p-4 border-gray-500 bg-white rounded-md  overflow-x-auto min-w-full`
+          ? `max-w-full m-2 p-4 border-gray-500 bg-white rounded-md  overflow-x-auto`
           : `w-full max-w-6xl m-2 p-4 border-gray-500 bg-white rounded-md`
       }
     >
-      {title && <h3 className="text-xl font-bold mt-1">{title}</h3>}
+      {title && !titleHref && (
+        <h3 className="text-xl font-bold mt-1">{title}</h3>
+      )}
+      {title && titleHref && (
+        <Link href={titleHref} passHref>
+          <a>
+            <h3 className="text-xl font-bold mt-1">{title}</h3>
+          </a>
+        </Link>
+      )}
       {subtitle && <h4 className="text-stone-600 font-semibold">{subtitle}</h4>}
       {children}
     </div>
