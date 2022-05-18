475f3d0bab915f1f1cd897b5082af3f28c38270b
janimaenpaa <itsmayd@gmail.com>
2022-05-18 21:25:30 +0300

    Add /api/login end point

---
+++ pages/api/login.ts
@@ -0,0 +1,37 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import nextConnect from "next-connect";
+import jwt from "jsonwebtoken";
+import cookie from "cookie";
+
+const handler = nextConnect<NextApiRequest, NextApiResponse>().post(
+  async (req, res) => {
+    const { password } = req.body;
+
+    if (password && password === process.env.ADMIN_PASSWORD) {
+      const token = jwt.sign(
+        { time: Date.now() },
+        process.env.JWT_SECRET as string,
+        { expiresIn: "8h" }
+      );
+
+      res.setHeader(
+        "Set-Cookie",
+        cookie.serialize("ACCESS_TOKEN", token, {
+          httpOnly: true,
+          maxAge: 8 * 60 * 60,
+          path: "/",
+          sameSite: "lax",
+          secure: process.env.NODE_ENV === "production",
+        })
+      );
+
+      res.status(200).json({ message: "Successful login!" });
+      return;
+    }
+
+    res.status(401).json({ error: "Wrong password." });
+    return;
+  }
+);
+
+export default handler;
