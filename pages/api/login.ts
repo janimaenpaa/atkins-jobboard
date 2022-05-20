147be108f20762e1a9c50550feb75f300825bffa
janimaenpaa <itsmayd@gmail.com>
2022-05-20 17:20:14 +0300

    Add password to response temporarily

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -31,7 +31,7 @@ const handler = nextConnect<NextApiRequest, NextApiResponse>().post(
 
     res
       .status(401)
-      .json({ error: "Wrong password.", password: process.env.PASSWORD });
+      .json({ error: "Wrong password.", password: process.env.ADMIN_PASSWORD });
     return;
   }
 );
